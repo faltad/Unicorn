6af0ee8a07f3111b50a12010261ca4f219d291a5
Faltad <faltad@gmail.com>
2010-07-02 14:38:15 +0200

    Added header files

---
+++ include/types.h
@@ -0,0 +1,18 @@
+/*
+** types.h for Unicorn in /home/faltad/projets/Unicorn/src
+** 
+** Made by Faltad
+** Login   <faltad@gmail.com>
+** 
+** Started on  Fri Jul  2 00:20:16 2010 Faltad
+** Last update Fri Jul  2 00:21:58 2010 Faltad
+*/
+
+#ifndef __TYPES_H_
+#define __TYPES_H_
+
+typedef unsigned int	uint;
+typedef unsigned short	ushort;
+typedef unsigned char	uchar;
+
+#endif
