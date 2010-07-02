221d5d39fdc84deab50aafba4f1ca2c2c7268049
Faltad <faltad@gmail.com>
2010-07-02 22:59:59 +0200

    Added the loading of the GDT, but it seems to triple-fault :)

--- include/descriptor_table.h
+++ include/descriptor_table.h
@@ -5,7 +5,7 @@
 ** Login   <faltad@gmail.com>
 ** 
 ** Started on  Thu Jul  1 22:37:43 2010 Faltad
-** Last update Fri Jul  2 21:55:35 2010 Faltad
+** Last update Fri Jul  2 22:39:14 2010 Faltad
 */
 
 
@@ -16,13 +16,12 @@
 
 typedef struct			s_gdt_entry
 {
-   ushort lim0_15;    
-   ushort base0_15;
-   uchar base16_23;
-   uchar acces;
-   uchar lim16_19 : 4;
-   uchar other : 4;
-   uchar base24_31;
+   ushort limit_low;
+   ushort base_low;
+   uchar  base_middle;
+   uchar  access;
+   uchar  granularity;
+   uchar  base_high;
 } __attribute__ ((packed))	t_gdt_entry;
 
 typedef struct			s_gdtptr
@@ -31,6 +30,5 @@ typedef struct			s_gdtptr
    uint				base;
 } __attribute__ ((packed))	t_gdtptr;
 
-#define GDTADDR			0x0
 
 #endif
--- src/gdt.c
+++ src/gdt.c
@@ -1,27 +1,48 @@
 
+#include "descriptor_table.h"
+#include "types.h"
+
 t_gdtptr	gdtptr;
 t_gdt_entry	gdt_entries[5];
 
-
-void init_gdt_desc(uint base, uint limite, uchar acces, uchar other,
-                   struct t_gdt_entry *desc)
+static void gdt_set_gate(int num, uint base, uint limit, uchar access, uchar gran)
 {
-   desc->lim0_15 = (limite & 0xffff);
-   desc->base0_15 = (base & 0xffff);
-   desc->base16_23 = (base & 0xff0000) >> 16;
-   desc->acces = acces;
-   desc->lim16_19 = (limite & 0xf0000) >> 16;
-   desc->other = (other & 0xf);
-   desc->base24_31 = (base & 0xff000000) >> 24;
-   return;
+    gdt_entries[num].base_low    = (base & 0xFFFF);
+    gdt_entries[num].base_middle = (base >> 16) & 0xFF;
+    gdt_entries[num].base_high   = (base >> 24) & 0xFF;
+
+    gdt_entries[num].limit_low   = (limit & 0xFFFF);
+    gdt_entries[num].granularity = (limit >> 16) & 0x0F;
+    
+    gdt_entries[num].granularity |= gran & 0xF0;
+    gdt_entries[num].access      = access;
+}
+
+
+void	gdt_flush(void)
+{
+   asm("lgdtl (gdtptr)");
+   asm("movw 0x10, %ax\n \
+        movw %ax, %ds\n \
+        movw %ax, %es\n \
+	movw %ax, %fs\n \
+        movw %ax, %gs\n \
+        movw %ax, %ss\n \
+	ljmp $0x08, $next\n \
+	next:		\n");
 }
 
 void	init_gdt(void)
 {
-   init_gdt_desc(0, 0, 0, 0, &gdt_entries[0]); // NULL segment 
-   init_gdt_desc(0, 0xFFFFFFFF, 0x9A, 0xCF, &gdt_entries[1]); // Kernel code 
-   init_gdt_desc(0, 0xFFFFFFFF, 0x92, 0xCF, &gdt_entries[2]); // Kernel data
-   gdtptr.base = GDTADDR;
-   gdtptr.limit = 3 * sizeof(t_gdt_entry) - 1;
-   memcpy((void *)gdtptr.base, (void *)gdt_entries, gdtptr.limit);
+
+    gdtptr.limit = (sizeof(t_gdt_entry) * 5) - 1;
+    gdtptr.base  = (uint)&gdt_entries;
+
+    gdt_set_gate(0, 0, 0, 0, 0);                // Null segment
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
+    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment
+    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment
+
+    gdt_flush();
 }
