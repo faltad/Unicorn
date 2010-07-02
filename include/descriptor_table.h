9a87403bd2bab9103d87cab0ecd85b80c26c0a1c
Faltad <faltad@gmail.com>
2010-07-02 22:10:42 +0200

    and ...

--- include/descriptor_table.h
+++ include/descriptor_table.h
@@ -5,7 +5,7 @@
 ** Login   <faltad@gmail.com>
 ** 
 ** Started on  Thu Jul  1 22:37:43 2010 Faltad
-** Last update Fri Jul  2 00:26:33 2010 Faltad
+** Last update Fri Jul  2 21:55:35 2010 Faltad
 */
 
 
@@ -16,12 +16,13 @@
 
 typedef struct			s_gdt_entry
 {
-   ushort			limit_low;
-   ushort			base_low;
-   uchar       			base_middle;
-   uchar       			granularity;
-   uchar       			access;
-   uchar       			base_high;
+   ushort lim0_15;    
+   ushort base0_15;
+   uchar base16_23;
+   uchar acces;
+   uchar lim16_19 : 4;
+   uchar other : 4;
+   uchar base24_31;
 } __attribute__ ((packed))	t_gdt_entry;
 
 typedef struct			s_gdtptr
@@ -29,5 +30,7 @@ typedef struct			s_gdtptr
    ushort			limit;
    uint				base;
 } __attribute__ ((packed))	t_gdtptr;
+
+#define GDTADDR			0x0
 
 #endif
